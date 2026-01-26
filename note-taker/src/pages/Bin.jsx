18efe3ae3b5c64bb0551c9288a8ddc87f7b7aa75
VishnuPS011 <vishnu.sureshbabu@sadhguru.org>
2026-01-26 14:30:10 +0530

    style: update folder icon color to macOS-style blue.

--- note-taker/src/pages/Bin.jsx
+++ note-taker/src/pages/Bin.jsx
@@ -127,7 +127,7 @@ const Bin = () => {
             {folders.map(folder => (
               <div key={folder._id} className="bin-item-wrapper">
                 <div className="bin-folder-preview card glass">
-                  <div className="folder-visual" style={{ width: '40px', height: '40px', color: '#fbbf24' }}>
+                  <div className="folder-visual" style={{ width: '40px', height: '40px', color: '#00a2ff' }}>
                     <svg viewBox="0 0 24 24" fill="currentColor">
                       <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" />
                     </svg>
